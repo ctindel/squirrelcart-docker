12ffb540b558c3300f9704cb7e5e023ff734af04
Chad Tindel <chad@tindel.net>
2018-05-03 05:11:54 +0000

    Build process initial working

--- docker_squirrelcart/src/install.sh
+++ docker_squirrelcart/src/install.sh
@@ -3,17 +3,17 @@
 SC_VER=squirrelcart-pro-v3.0.1
 SC_TARBALL=$SC_VER.tar.gz
 SERVER_ROOT=/project
-tmp_dir=/tmp/sc
+TMP_DIR=/tmp/sc
 
 mkdir -p $SERVER_ROOT
-tar zxpvf $tmp_dir/$SC_TARBALL -C $tmp_dir
-mv $tmp_dir/$SC_VER/* $SERVER_ROOT
+tar zxpvf $TMP_DIR/$SC_TARBALL -C $TMP_DIR
+mv $TMP_DIR/$SC_VER/* $SERVER_ROOT
 
 #https://www.squirrelcart.com/help/3.2.0/Setting%20File%20Permissions.html
 find $SERVER_ROOT/sc_images -type d | xargs chmod 777
 chmod 777 $SERVER_ROOT/squirrelcart/sc_data
 chmod 777 $SERVER_ROOT/squirrelcart/config.php
-cp /tmp/src/config.php /project/squirrelcart
+cp $TMP_DIR/src/config.php /project/squirrelcart
 rm -rf /project/sc_install
 chown -R www-data:www-data $SERVER_ROOT/*
 
